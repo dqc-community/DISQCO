5d00677efd73002bc9622307e2116ea248fcf40b
Elie <eliebenshlomo@outlook.com>
2026-04-15 21:44:55 +1000

    Responding to feedback

--- benchmarking/qasmbench_hyperedge_cost.py
+++ benchmarking/qasmbench_hyperedge_cost.py
@@ -6,6 +6,8 @@ import pathlib
 import numpy as np
 from disqco import QuantumCircuitHyperGraph, QuantumNetwork
 from disqco.graphs.hypergraph_methods import calculate_full_cost
+from disqco.graphs.coarsening.coarsener import HypergraphCoarsener
+from disqco.parti import FiducciaMattheyses, set_initial_partition_assignment
 from disqco.parti.partitioner import QuantumCircuitPartitioner
 from qiskit import QuantumCircuit, qasm2, transpile
 
@@ -14,7 +16,7 @@ CIRCUITS_DIR = pathlib.Path(__file__).parent / "circuits"
 # (qubits_per_node, nodes)
 CASES: dict[str, tuple[int, int]] = {
     "qft_n18":          (9,  2),
-    # "square_root_n18":  (9,  2),
+    "square_root_n18":  (9,  2),
     "adder_n28":        (14, 2),
     "ising_n34":        (17, 2),
     "ghz_n40":          (20, 2),
@@ -45,6 +47,9 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--num-passes", type=int, default=20)
     parser.add_argument("--partitioner", choices=["fm", "fgp", "genetic"], default="fm")
+    parser.add_argument("--passes-per-level", type=int, default=10)
+    parser.add_argument("--spare-qubits-per-node", type=int, default=1)
+    parser.add_argument("--level-limit", type=int, default=None)
     args = parser.parse_args()
 
     rows: list[tuple[str, int, int, int]] = []
@@ -52,13 +57,26 @@ def main() -> None:
     for circuit_id, (qpn, n_nodes) in CASES.items():
         qasm_text = (CIRCUITS_DIR / f"{circuit_id}.qasm").read_text()
         qc = _normalize(qasm2.loads(qasm_text))
-
-        net = QuantumNetwork.create([qpn] * n_nodes, "all_to_all")
-        part = QuantumCircuitPartitioner.create(args.partitioner, qc, net)
-        res = part.partition(num_passes=args.num_passes, log=False)
-        assignment = np.asarray(res["best_assignment"], dtype=int)
-
         hg = QuantumCircuitHyperGraph(qc)
+
+        net = QuantumNetwork.create(
+            [qpn + args.spare_qubits_per_node] * n_nodes,
+            "all_to_all",
+        )
+
+        if args.partitioner == "fm":
+            initial_assignment = set_initial_partition_assignment(hg, net)
+            partitioner = FiducciaMattheyses(qc, net, initial_assignment, hypergraph=hg)
+            res = partitioner.multilevel_partition(
+                coarsener=HypergraphCoarsener().coarsen_recursive_batches_mapped,
+                passes_per_level=args.passes_per_level,
+                level_limit=args.level_limit,
+            )
+        else:
+            partitioner = QuantumCircuitPartitioner.create(args.partitioner, qc, net)
+            res = partitioner.partition(num_passes=args.num_passes, log=False)
+
+        assignment = np.asarray(res["best_assignment"], dtype=int)
         hyperedge_cost = calculate_full_cost(hg, assignment, n_nodes)
 
         rows.append((circuit_id, n_nodes, qpn, hyperedge_cost))
